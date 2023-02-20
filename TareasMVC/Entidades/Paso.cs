94a26427c3883f207cde8bea8404ac10b6dfa5e6
Elejota <luiscarloslealjimenez@hotmail.com>
2023-02-20 14:48:50 -0500

    Version1 proyecto

---
+++ TareasMVC/Entidades/Paso.cs
@@ -0,0 +1,12 @@
+﻿namespace TareasMVC.Entidades
+{
+    public class Paso
+    {
+        public Guid Id { get; set; }
+        public int TareaId { get; set; }  //Esta es la llave foranea        
+        public Tarea Tarea { get; set; } //Propiedad de navegación, me permite cargar la data relacionada de la tarea como un comboBox
+        public string Descripcion { get; set; }
+        public bool Realizado { get; set; }
+        public int Orden { get; set; }
+    }
+}
---
+++ TareasMVC/Entidades/Tarea.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace TareasMVC.Entidades
+{
+    public class Tarea
+    {
+        //lave primaria
+        public int Id { get; set; }
+        [StringLength(250)]
+        [Required]
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int Orden { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public List<Paso> Pasos { get; set; } //Acá cargo los pasos que corresponde a esa tarea.
+
+        //Una tarea tiene muchos pasos y un paso tiene solo una tarea
+
+        public List<ArchivoAdjunto> ArchivoAdjuntos { get; set; }
+
+    }
+}
